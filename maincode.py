bd4177bf3b3c6e745c55ff45dbd53d54c06160f9
SergeiUser <student.sergei.vasilev10@ctyi.org>
2025-03-19 14:35:30 +0000

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -14,7 +14,19 @@ WINDOW_HEIGHT = 900
 WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Nomekop!')
 
+enemyx = 800
+enemyy = 450
+
+looping = True
+
+While looping:
+  enemyx-=10
+
 
 WINDOW.fill(BACKGROUND)
+
+enemy = pygame.draw.circle(WINDOW, (255,0,0), (enemyx,enemyy), 20)
 pygame.display.update()
   fpsClock.tick(FPS)
+
+
