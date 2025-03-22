5d1317f905629c097dcecbfaa4a96e345c9a906c
Astralis17 <aurora.spark17@gmail.com>
2025-03-22 23:18:22 +0000

    commit #2

--- testing.py
+++ testing.py
@@ -1,2 +1,59 @@
 import towers
+import pygame, random
 
+windowDimensions = (1080, 605)
+gridSize = (16, 9)
+
+cellSize = [windowDimensions[x]/gridSize[x] for x in range(len(gridSize))]
+print(cellSize)
+pygame.init()
+
+window = pygame.display.set_mode(windowDimensions)
+
+#tower = towers.arrow(window, position=[8, 0], cellSize=cellSize)
+towersInScene = [
+        towers.arrow(
+                window,
+                position=[random.randint(0,gridSize[0]-1), random.randint(2,gridSize[1]-2)],
+                cellSize=cellSize)
+                for x in range(6)]
+
+for x in range(len(towersInScene)):
+        print(f"Tower {x} is: {type(towersInScene[x])}")
+cursor = towers.cursor(0,0)
+
+enemies = [towers.enemy(
+        window,
+        x = windowDimensions[0] * random.random(),
+        y = (windowDimensions[1]/3) * random.random() + windowDimensions[1]/3,
+        cellSize=cellSize,
+        speed = random.uniform(3,6)
+        ) for x in range(6)]
+enemies.append(cursor)
+run = True
+while run:
+        cursor.position = pygame.mouse.get_pos()
+        window.fill("darkblue")
+        pygame.time.delay(5)
+
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                        run = False
+
+        #tower.position[0] = (tower.position[0] + 1) % (gridSize[0] + 2)
+
+        for tower in towersInScene:
+                pygame.draw.circle(tower.surface, "darkgreen", (tower.drawX + tower.cellSize[0]/2, tower.drawY + tower.cellSize[1]/2), tower.range)
+        for tower in towersInScene:
+                tower.draw()
+                tower.findClosestTarget(enemies)
+
+        x = 0
+        for enemy in enemies:
+                enemy.draw()
+                enemy.sim()
+                if enemy.health < 0:
+                        del enemies[x]
+                else:
+                        x += 1
+        pygame.display.update()
--- towers.py
+++ towers.py
@@ -1,7 +1,10 @@
 # File for containing the classes for all of the towers in the game
 from pygame import draw as pygameDraw
+from math import dist
+from random import randint
 
 typeColour = {
+        "base" : (90, 90, 90),
         "arrow": (137, 81, 41),
         "fire" : (232, 97 ,0),
         "chain": (70, 130, 180)
@@ -9,27 +12,116 @@ typeColour = {
 
 
 
+class cursor:
+        health = 1000
+        def __init__(self, x,y):
+                self.x = x
+                self.y = y
+                self.position = (x,y)
 
+        def draw(self):
+                pass
+
+        def sim(self):
+                print(f"Health: {self.health}")
 
 class towerBase:
-
-        def __init__(self, type, x=0, y=0, position=[], cellSize=10):
-                self.type = type
-
-
-
-
-
+        range = 150
+        cooldown = 500
+        cooldownCounter = cooldown
+        cooldownSpeed = 2.5
+        drawLine = False
+        type = "base"
+        def __init__(self, surface, x=0, y=0, position=[], cellSize=[10,10]):
+                self.surface = surface
+                self.cellSize = cellSize
                 if len(position) != 2:
-                        self.position = [x, y]
+                        self.position = (x, y)
                 else:
-                        self.position = position
+                        self.position = tuple(position)
+                self.drawX = self.position[0] * self.cellSize[0]
+                self.drawY = self.position[1] * self.cellSize[1]
 
-        def drawBase(self, surface, ):
-                drawX = self.position[0] * self.cellSize
-                drawY = self.position[1] * self.cellSize
-                pygameDraw.rect(surface, typeColour[self.type], (drawX, drawY, self.cellSize, self.cellSize))
+        def drawBase(self):
+                self.drawX = self.position[0] * self.cellSize[0]
+                self.drawY = self.position[1] * self.cellSize[1]
+                pygameDraw.rect(self.surface, typeColour[self.type], (self.drawX, self.drawY, self.cellSize[0], self.cellSize[1]))
+
+
+
+        def findClosestTarget(self, targets):
+                position = ((self.position[0] * self.cellSize[0]) + self.cellSize[0]/2, (self.position[1] * self.cellSize[1]) + self.cellSize[1]/2)
+                target = enemy(self.surface, position=position, health=10000000, speed=0)
+
+                for t in targets:
+                        if target.health > 100000:
+                                target = t
+                                self.drawLine = False
+                        else:
+                                self.drawLine = True
+                                if dist(position, t.position) < dist(position, target.position):
+                                        target = t
+                targetPos = target.position
+                if self.cooldownCounter < 50 and dist(targetPos, position) < self.range:
+                        if target.health < 100000:
+                                self.drawLine = True
+                        if self.cooldownCounter < 0 and dist(targetPos, position) < self.range:
+                                self.cooldownCounter = self.cooldown
+                                target.health -= self.damage
+                        else:
+                                self.cooldownCounter -= self.cooldownSpeed
+                else:
+                        self.drawLine = False
+                        self.cooldownCounter -= self.cooldownSpeed
+                if self.drawLine:
+                        pygameDraw.line(self.surface, "green", position, targetPos, 4)
 
 
 class arrow(towerBase):
-        pass
+        type = "arrow"
+        damage = 2
+
+        def draw(self):
+                self.drawBase()
+                pygameDraw.polygon(self.surface, (255, 255, 255),(
+                        (self.drawX + (self.cellSize[0]/2), self.drawY +5),
+                        (self.drawX -5 + self.cellSize[0], self.drawY + (self.cellSize[1]/2)),
+                        (self.drawX + (self.cellSize[0]/2), self.drawY + self.cellSize[1] -5),
+                        (self.drawX +5, self.drawY + (self.cellSize[1]/2))
+                ))
+                pygameDraw.rect(self.surface, (55,55,55), (
+                        self.drawX + (self.cellSize[0]/3), self.drawY + (self.cellSize[1]/3),
+                        self.cellSize[0]/3, self.cellSize[0]/3
+                        ))
+
+
+class enemy:
+
+        def __init__(self, surface, x=0, y=0, position=[], health=10, damage=5, speed=5, cellSize=[10,10]):
+                self.surface = surface
+                self.cellSize = cellSize
+                self.health = health
+                self.damage = damage
+                self.speed = speed
+                if len(position) != 2:
+                        self.position = (x, y)
+                else:
+                        self.position = tuple(position)
+
+                self.colour = (
+                        randint(10, 255),
+                        randint(10, 255),
+                        randint(10, 255)
+                )
+
+
+
+        def draw(self):
+
+                pygameDraw.circle(self.surface, self.colour, self.position, 5)
+
+        def sim(self):
+                self.position = ((self.position[0] + self.speed) % self.surface.get_width(), self.position[1])
+                if self.health < 0:
+                        self.colour = (0,0,0)
+
