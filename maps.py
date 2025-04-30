59e25bf104ac20788699cda899b04e6dfbdb67aa
SergeiUser <student.sergei.vasilev10@ctyi.org>
2025-04-30 13:02:50 +0100

    aight go

--- maps.py
+++ maps.py
@@ -19,16 +19,16 @@ maps =[
         [G,     G, G, G, G,    G, G, G, G, G,     G, G, G,(RT,0), RH,(RT,3),G,N,N,N],#9
     ],
     [
-        [G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G,N,N,N],#0
-        [G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G,N,N,N],#1
-        [G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G,N,N,N],#2
-        [G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G,N,N,N],#3
-        [G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G,N,N,N],#4
-        [G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G,N,N,N],#5
-        [G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G,N,N,N],#6
-        [G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G,N,N,N],#7
-        [G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G,N,N,N],#8
-        [G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G,N,N,N],#9
+        [G, G, G, G,           G, G, G, G,        G, G, G, G, G, G,          G,RV,     G,N,N,N],#0
+        [G, G, G, G,           G, G, G,(RT,1),    RH,RH,RH,RH,RH,(RT,2),     G,RV,     G,N,N,N],#1
+        [RH,RH,RH,(RT,2),      G, G, G,RV,        G, G, G, G, G,RV,          G,RV,     G,N,N,N],#2
+        [G, G, G, RV,          G, G, G,RV,        G, G, G, G, G,RV,          G,RV,     G,N,N,N],#3
+        [G, G, G, RV,          G, G, G,RV,        G, G, G, G, G,RV,          G,RV,     G,N,N,N],#4
+        [G, G, G, RV,          G, G, G,(RT,0),   RH,RH,RH,RH,RH,RC,         RH,(RT,3), G,N,N,N],#5
+        [G, G, G, RV,          G, G, G, G,        G, G, G, G, G,RV,          G, G,     G,N,N,N],#6
+        [G, G, G, RV,          G, G, G, G,        G, G, G, G, G,RV,          G, G,     G,N,N,N],#7
+        [G, G, G, (RT,0),      RH,RH,RH,RH,       RH,RH,RH,RH,RH,(RT,3),     G, G,     G,N,N,N],#8
+        [G, G, G, G,           G, G, G, G,        G, G, G, G, G, G,          G, G,     G,N,N,N],#9
 
     ],
 ]
